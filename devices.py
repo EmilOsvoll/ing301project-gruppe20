adc81c0952008dba25b020d47a2d550b08d09d1d
Someoneeelsse <panjakub15@gmail.com>
2023-02-15 11:29:49 +0100

    json devices

--- devices.py
+++ devices.py
@@ -1,21 +1,4 @@
 class Device:
 
-    # Using Dictionary: 
- 
-    # Bool: If registered: 
-    dev1 = ["Smart Lys", "Fritsch Group", "Tresom Bright 1.0", "f11bb4fc-ba74-49cd", True]
-    dev2 = ["Smart Lys", "Fritsch Group", "Alphazap 2", "480dbae8-cce7-46d7", False]
-    dev3 = ["Fuktighetssensor", "Bernhard-Roberts", "Andalax", "4cb686fe-6448-4cf6", False]
-    dev4 = ["Smart Lys", "Larkin-Nitzsche", "Alphazap 2", "6a36c71d-4f48-4eb4"]
-    dev5 = ["Smart Lys", "Fritsch Group", "Tresom Bright 1.0", "f11bb4fc-ba74-49cd"]
-    dev6 = ["Smart Lys", "Fritsch Group", "Tresom Bright 1.0", "f11bb4fc-ba74-49cd"]
-    dev7 = ["Smart Lys", "Fritsch Group", "Tresom Bright 1.0", "f11bb4fc-ba74-49cd"]
-    dev8 = ["Smart Lys", "Fritsch Group", "Tresom Bright 1.0", "f11bb4fc-ba74-49cd"]
-    dev9 = ["Smart Lys", "Fritsch Group", "Tresom Bright 1.0", "f11bb4fc-ba74-49cd"]
-    dev10 = ["Smart Lys", "Fritsch Group", "Tresom Bright 1.0", "f11bb4fc-ba74-49cd"]
-    dev11 = ["Smart Lys", "Fritsch Group", "Tresom Bright 1.0", "f11bb4fc-ba74-49cd"]
-    dev12 = ["Smart Lys", "Fritsch Group", "Tresom Bright 1.0", "f11bb4fc-ba74-49cd"]
-    sensor = {[dev1], ...}
-    aktuator = {[dev3], ...}
 
 # TODO! Her skal du utvikle din egen design av en klassestruktur med enheter og deres funkjsoner!
