2de81d49b106204f93e6b8a2e55b238083e3b02a
Someoneeelsse <panjakub15@gmail.com>
2023-02-15 11:37:08 +0100

    changes

--- devices.py
+++ devices.py
@@ -1,4 +1,33 @@
+import json 
+f = open('dev.json')
+
+
+
 class Device:
+
+    def __init__(self): 
+        self.devices = json.load(f)
+
+
+
+    class Sensor: 
+        
+        def __init__(self):
+            self.sensors = [] 
+
+
+        def add_sensors(self, sensors): 
+
+
+
+
+    class Actuator: 
+
+        def __init__(self): 
+            self.actuators = []
+
+
+
 
 
 # TODO! Her skal du utvikle din egen design av en klassestruktur med enheter og deres funkjsoner!
